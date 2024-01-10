e581d9f4da1e9c4d5d28142245e5a4158047e7ac
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-10 12:41:11 -0500

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,17 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.knowledge.append(string)
+        return self.knowledge
+        
+
+tyler = Student('tyler', 'kim')
+print(tyler.first_name, tyler.knowledge)
+    # Expand the learn() method in the Student class so that in takes in a
+    #  string and adds that string
+    #  to the student's self.knowledge list.
